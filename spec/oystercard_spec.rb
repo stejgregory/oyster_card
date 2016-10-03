237ef06477b65dc8d79fdf5961c303e27a3d39d1
hanrattyjen <hanratty.jen@gmail.com>
2016-10-03 15:19:25 +0100

    Create a unit test for checking default balance of card.
    Create initialized balance method for Oystercard class.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,13 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) {described_class.new}
 
-  
+  it { is_expected.to respond_to :balance }
+
+  describe 'initialization' do
+    it 'has a default balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
