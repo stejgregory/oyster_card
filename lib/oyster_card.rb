791ab4d24d4d7b9d786eb65c5a8ce0deaf453f4f
Stephen Gregory <stejgregory@gmail.com>
2016-10-03 15:26:41 +0100

    topup methods added + limit on card balance

---
+++ lib/oyster_card.rb
@@ -0,0 +1,20 @@
+class Oystercard
+DEFAULT_BALANCE = 0
+
+  def initialize
+    @balance = DEFAULT_BALANCE
+  end
+
+  def check_balance
+    @balance
+  end
+
+  def topup(funds)
+    if (@balance + funds) > 90
+    "Error, this will exceed the £90 maximum balance."
+    else
+    @balance = @balance + funds
+    end
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,28 @@
-describe Oystercard do
+require 'oyster_card'
 
-  
+describe Oystercard do
+  # let(:oyster_card) { double :Oystercard.new }
+  it { is_expected.to respond_to :check_balance}
+
+  it "checks that opening balance = 0" do
+  expect(Oystercard::DEFAULT_BALANCE).to eq(0)
+  end
+
+  it "checks that a newly created card has a balance of 0" do
+    card = Oystercard.new
+    expect(card.check_balance).to eq 0
+  end
+
+  it "tops up the card with additional funds, and increases the balance by that amount" do
+    card = Oystercard.new
+    card.topup(10)
+    expect(card.check_balance).to eq 10
+  end
+
+  # it "sets a maximum topup limit of 90 pounds sterling" do
+  #   card = Oystercard.new
+  #   card.topup(90)
+  #   expect(card.topup(1)).to eq "Error, this will exceed the £90 maximum balance."
+  # end
+
 end
