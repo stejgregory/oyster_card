96b0d3be105608361977a0f4179cb2b6349960af
andy jenkins <andygjenkins@gmail.com>
2016-10-03 16:16:24 +0100

    Refactor .top_up and .deduct tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,6 +6,23 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
+
+  context 'With max balance on card' do
+    before do
+      subject.top_up(described_class::MAXIMUM_BALANCE)
+    end
+
+    it 'can not be topped up beyond limit' do
+      message = "Beyond limit of #{described_class::MAXIMUM_BALANCE}"
+      expect {subject.top_up(1)}.to raise_error message
+    end
+
+    it 'can deduct the balance' do
+      expect{ subject.deduct 2}.to change{ subject.balance }.by -2
+    end
+
+  end
+
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
@@ -13,21 +30,13 @@ describe Oystercard do
       expect{ subject.top_up 1}.to change{ subject.balance }.by 1
     end
 
-    it 'can not be topped up beyond limit' do
-      message = "Beyond limit of #{described_class::MAXIMUM_BALANCE}"
-      subject.top_up(described_class::MAXIMUM_BALANCE)
-      expect {subject.top_up(1)}.to raise_error message
-    end
 
   end
 
   describe '#deduct' do
     it { is_expected.to respond_to(:deduct).with(1).argument }
 
-    it 'can deduct the balance' do
-      subject.top_up 5
-      expect{ subject.deduct 2}.to change{ subject.balance }.by -2
-    end
+
   end
 
   describe '#in_journey?' do
