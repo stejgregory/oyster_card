1019b1b6f3616b87ff13a798fd9268ef1d5ed8cb
andy jenkins <andygjenkins@gmail.com>
2016-10-03 14:55:09 +0100

    Complete ch5 add .top_up and test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -7,4 +7,8 @@ class Oystercard
     @balance = 0
   end
 
+  def top_up(money)
+    @balance += money
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,5 +6,13 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect{ subject.top_up 1}.to change{ subject.balance }.by 1
+    end
+
+  end
 
 end
