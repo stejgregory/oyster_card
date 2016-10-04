e9c2699810851639d465dfd285747e1a0a3d8ba6
hanrattyjen <hanratty.jen@gmail.com>
2016-10-04 10:42:12 +0100

    Create test and method for in_journey
    Create test and method for touch_in
    
    Create test and method for touch_out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,19 +1,28 @@
 class Oystercard
   attr_reader :balance
+  attr_accessor :in_journey
 
   MAXIMUM_BALANCE = 90
 
   def initialize(balance = 0)#, limit = self.class::DEFAULT_LIMIT)
     @balance = balance
-    # @limit = limit
+    @in_journey = false
   end
 
   def top_up(amount)
     fail "Card limit of £#{MAXIMUM_BALANCE} has been reached." if (@balance + amount) > MAXIMUM_BALANCE
     @balance += amount
   end
-  
+
   def deduct(amount)
     @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,24 +9,47 @@ describe Oystercard do
     end
   end
 
-  it 'allows a user to top-up their oystercard' do
-    card = Oystercard.new
-    card.top_up(40)
-    expect(card.balance).to eq 40
-  end
-  
-  it 'allows a fare to be deducted from their oystercard' do
-    card = Oystercard.new
-    card.top_up(40)
-    card.deduct(10)
-    expect(card.balance).to eq 30
+  context 'top-up card' do
+    before do
+      subject.top_up(described_class::MAXIMUM_BALANCE)
+    end
+    it 'allows a user to top-up their oystercard' do
+      expect(subject.balance).to eq described_class::MAXIMUM_BALANCE
+    end
+
+    it 'will raise an error if maximum card value is reached' do
+      message = "Card limit of £#{described_class::MAXIMUM_BALANCE} has been reached."
+      expect{ subject.top_up(1) }.to raise_error message
+    end
   end
 
-  it 'will raise an error if maximum card value is reached' do
-    maximum_balance = described_class::MAXIMUM_BALANCE
-    card = Oystercard.new
-    card.top_up(maximum_balance)
-    message = "Card limit of £#{maximum_balance} has been reached."
-    expect{ card.top_up(1) }.to raise_error message
+  it 'allows a fare to be deducted from their oystercard' do
+    subject.top_up(described_class::MAXIMUM_BALANCE)
+    subject.deduct(10)
+    expect(subject.balance).to eq 80
+  end
+
+  describe '#in_journey?' do
+    it 'will be initially set to false' do
+      expect(subject.in_journey).to eq false
+    end
+  end
+
+  context 'using a card' do
+    before do
+      subject.touch_in
+    end
+    describe '#touch_in' do
+      it 'will set #in_journey? to true' do
+        expect(subject.in_journey).to eq true
+      end
+    end
+
+    describe '#touch_out' do
+      it 'will set #in_journey? to false' do
+        subject.touch_out
+        expect(subject.in_journey).to eq false
+      end
+    end
   end
 end
