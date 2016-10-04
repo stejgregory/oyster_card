38cf05e3e62b458dc208c3cc7b5eb9aea3b0ba67
andy jenkins <andygjenkins@gmail.com>
2016-10-04 10:03:18 +0100

    Refactor rspec tests - first attempt

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,5 +1,10 @@
 class Oystercard
-DEFAULT_BALANCE = 0.0
+
+  DEFAULT_BALANCE = 0.0
+
+
+
+  attr_reader :balance
 
   def initialize
     @balance = DEFAULT_BALANCE
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,48 +4,54 @@ describe Oystercard do
   let(:card) { (Oystercard.new) }
   it { is_expected.to respond_to :check_balance}
 
-  it "checks that opening balance = 0" do
-  expect(Oystercard::DEFAULT_BALANCE).to eq(0)
+  it "should have an opening balance = 0" do
+    expect(Oystercard::DEFAULT_BALANCE).to eq(0)
   end
 
-  it "checks that a newly created card has a balance of 0" do
-    expect(card.check_balance).to eq 0
+
+  describe '#in_journey'  do
+    it "will know when the card is in journey" do
+      card.topup(1.0)
+      card.touch_in?
+      expect(card.in_journey?).to eq true
+    end
+
   end
 
-  it "tops up the card with additional funds, and increases the balance by that amount" do
-    card.topup(10)
-    expect(card.check_balance).to eq 10
+
+  describe '#touch_in' do
+    it "will know when the card has been used to touch-in" do
+      card.topup(1.0)
+      expect(card.touch_in?).to eq true
+    end
+
+    it "will onll allow a card to touch_in if it has a balance >= £1" do
+      card.topup(0.5)
+      expect(card.touch_in?).to eq "Not enough funds on card."
+    end
+
   end
 
-  it "sets a maximum topup limit of 90 pounds sterling" do
-    card.topup(90)
-    expect(card.topup(1)).to eq "Error, this will exceed the £90 maximum balance."
+
+  describe '#touch_out' do
+
+    it "will know when the card has been used to touch-out" do
+      expect(card.touch_out?).to eq false
+    end
+
   end
 
-  it "will reduce the card's balance when money is withdrawn or used" do
-    card.topup(45)
-    card.deduct(10)
-    expect(card.check_balance).to eq(35)
-  end
 
-  it "will know when the card has been used to touch-in" do
-    card.topup(1.0)
-    expect(card.touch_in?).to eq true
-  end
+  describe '#top_up' do
 
-  it "will know when the card has been used to touch-out" do
-    expect(card.touch_out?).to eq false
-  end
+    it "tops up the card with additional funds, and increases the balance by that amount" do
+      card.topup(10)
+      expect(card.check_balance).to eq 10
+    end
 
-  it "will know when the card is in journey" do
-    card.topup(1.0)
-    card.touch_in?
-    expect(card.in_journey?).to eq true
+    it "sets a maximum topup limit of 90 pounds sterling" do
+      card.topup(90)
+      expect(card.topup(1)).to eq "Error, this will exceed the £90 maximum balance."
+    end
   end
-
-  it "will onll allow a card to touch_in if it has a balance >= £1" do
-  card.topup(0.5)
-  expect(card.touch_in?).to eq "Not enough funds on card."
-  end
-
 end
