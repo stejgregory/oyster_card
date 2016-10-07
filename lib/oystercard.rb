e51d5d28fc95c9e68df5516a24e841f52632fbf6
B-raw <michaelbarry1989@hotmail.com>
2016-10-07 08:55:31 +0100

    Fix oystercard logic

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -21,29 +21,14 @@ class Oystercard
 
   def touch_in(entry_station)
     fail "Insufficient balance" if balance < MINIMUM_FARE
-    double_touch_in(entry_station) if @current_journey # (!@journey_log.empty? && !@journey_log.last.complete?)
+    double_touch_in if @current_journey # (!@journey_log.empty? && !@journey_log.last.complete?)
     # charge_penalty_fare(nil) if @current_journey # (!@journey_log.empty? && !@journey_log.last.complete?) ##CHANGE ME
     @current_journey = Journey.new(entry_station)
   end
 
   def touch_out(exit_station)
-
-
-
-    if !@current_journey #(!@journey_log.empty? && !@current_journey)
-      double_touch_out
-
-      charge_penalty_fare(exit_station) # if (!@journey_log.empty? && !@current_journey)
-
-
-    else
-      end_current_journey(exit_station)
-    end
-
-
-    @current_journey.end(exit_station)
-    deduct(@current_journey.fare)
-    @current_journey = nil
+    double_touch_out(exit_station) if !@current_journey
+    end_current_journey(exit_station)
   end
 
   def journey_history
@@ -57,33 +42,18 @@ class Oystercard
   end
 
 
-  def double_touch_out
+  def double_touch_out exit_station
     @current_journey = Journey.new
-    deduct(@current_journey.fare)
-    end_current_journey(exit_station)
-
   end
 
 
   def double_touch_in
-    deduct(PENALTY_FARE)
-    end_current_journey(nil) #double touch in
+    end_current_journey(nil)
   end
 
-
-  # def charge_penalty_fare(exit_station)
-  #   if @current_journey
-  #
-  #   elsif !@current_journey #double touch out
-  #     @current_journey = Journey.new
-  #     end_current_journey(exit_station)
-  #   end
-  #   # @balance -= @current_journey[:fare]
-  # end
-
-  # def end_current_journey(exit_station)
-  #   @current_journey.end(exit_station)
-  #   deduct(@current_journey.fare)
-  #   @current_journey = nil
-  # end
+  def end_current_journey(exit_station)
+    @current_journey.end(exit_station)
+    deduct(@current_journey.fare)
+    @current_journey = nil
+  end
 end
